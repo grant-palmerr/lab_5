b1359a7b7b29d7e4f5ccdbf1787f76384cd3e0ec
maxlink <max.durham.link@gmail.com>
2025-03-07 13:26:48 -0600

    git init intelliJ

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+ return (
+     <div>
+       <h1>Employee Management</h1>
+       <EmployeeList />
+     </div>
+ );
+}
+
+export default App;
+
